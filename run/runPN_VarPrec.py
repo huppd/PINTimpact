fbd41c9e516530ffbfe95bb3ce7ae455ce452799
Daniel Hupp <huppd@inf.ethz.ch>
2015-03-06 13:35:52 +0000

    added runPN_VarPrec.py

---
+++ run/runPN_VarPrec.py
@@ -0,0 +1,53 @@
+import os
+from numpy import linspace
+import numpy as np
+from pylab import pi
+from platform_paths import *
+
+
+exe = 'peri_navier'
+
+
+os.chdir( exe_path )
+os.system( 'make '+exe+' -j4' )
+
+case_path = ['','','','','']
+npx= 1 
+npy= 1
+npt= 1
+
+
+case_consts = ' --npx='+str(npx)+' --npy='+str(npy)+' --npf='+str(npt)+' --tolNOX=1.e-6  --tolBelos=1.e-4  --maxIter=5  --lx=2. --ly=2.  --initZero=0 --tolInnerBelos=1.e-6 --numCycles=2 --maxGrids=4 '
+
+precTypes = [ 0, 1, 2, 3 ]
+ns        = [ 4, 5, 6 ]
+res       = [ 1, 10, 100 ]
+alpha2s   = [ 1, 10, 100 ]
+
+#data_path = '/cluster/scratch_xp/public/huppd'
+
+#ns  = [ 7 ]
+
+for alpha2 in alpha2s:
+	case_path[0] = '/a2_'+str(alpha2)
+	if not os.path.exists( data_path+case_path[0] ):
+		os.mkdir( data_path+case_path[0] )
+	for re in res:
+		case_path[1] = '/re_'+str(re)
+		if not os.path.exists( data_path+case_path[0]+case_path[1] ):
+			os.mkdir( data_path+case_path[0]+case_path[1] )
+		for n in ns:
+			case_path[2] = '/n2_'+str(n)
+			if not os.path.exists( data_path+case_path[0]+case_path[1]+case_path[2] ):
+				os.mkdir( data_path+case_path[0]+case_path[1]+case_path[2] )
+			for precType in precTypes:
+				case_path[3] = '/precType_'+str(precType)
+				if not os.path.exists( data_path+case_path[0]+case_path[1]+case_path[2]+case_path[3]   ):
+					os.mkdir( data_path+case_path[0]+case_path[1]+case_path[2]+case_path[3] )
+				os.chdir( data_path+case_path[0]+case_path[1]+case_path[2]+case_path[3] )
+				os.system(' rm ./* -r -v  ')
+				case_consts = ' --npx='+str(npx)+' --npy='+str(npy)+' --npf='+str(npt)+' --tolNOX=1.e-6  --tolBelos=1.e-4  --maxIter=5  --lx=2. --ly=2.  --initZero=0 --tolInnerBelos=1.e-6 --numCycles=2 --maxGrids=3 '
+				case_para = ' --nx='+str(2**n+1)+' --ny='+str(2**n+1)+' --nf='+str(8)+' --withprec='+str(precType)+'  --re='+str(re)+' --alpha2='+str(alpha2)+'  ' 
+				#os.system( exe_pre(npx*npy*npt,' -R lustre ')+exe_path+exe+case_para+case_consts )
+				os.system( exe_pre(npx*npy*npt)+exe_path+exe+case_para+case_consts +' > output ' )
+				print( exe_pre(npx*npy*npt)+exe_path+exe+case_para+case_consts +' > output ' )
