2a5843ca4064df55c5a4301eb8f984537247c796
Daniel Hupp <huppd@inf.ethz.ch>
2015-05-06 09:10:13 +0000

    bla

--- run/runPN3D_weak.py
+++ run/runPN3D_weak.py
@@ -7,6 +7,7 @@ from platform_paths import *
 
 #data_path = '/cluster/home04/math/huppd/trunk/data/'
 #data_path = '/cluster/scratch_xp/public/huppd'
+#data_path = '/cluster/scratch_xp/public/huppd'
 exe = 'peri_navier3D'
 
 
@@ -19,8 +20,8 @@ npy= 1
 npt= 1
 
 
-case_consts = [' --baseflow=1 --flow=17 --alpha2=9 --re=1500  --domain=1 --nf=8  --tolNOX=1.e-6 ',
-' --nf=8 --maxGrids=10  --baseflow=22 --flow=0 --re=400 --alpha2=9 --tolNOX=1.e-6  --tolBelos=1.e-1 --tolInnerBelos=1.e-3 --maxIter=20  --lx=30. --ly=30. --ly=30 --initZero=0 --numCycles=2 --domain=0 ']
+case_consts = [' --baseflow=1 --flow=17 --alpha2=9 --re=1500  --domain=1 --nf=4  --tolNOX=1.e-6 ',
+' --nf=4   --baseflow=22 --flow=0 --re=400 --alpha2=9 --tolNOX=1.e-6  --tolBelos=1.e-1 --tolInnerBelos=1.e-3 --maxIter=20  --lx=30. --ly=30. --ly=30 --initZero=0 --numCycles=2 --domain=0 ']
 
 ns        = [ 1,2,4 ]
 #res       = [ 1, 25, 50, 75, 100 ]
@@ -34,7 +35,6 @@ ns        = [ 1,2,4 ]
 #res       = [ 400 ]
 #alpha2s   = [ 125 ]
 
-#data_path = '/cluster/scratch_xp/public/huppd'
 
 #ns  = [ 6, 7 ]
 
@@ -42,7 +42,7 @@ ns        = [ 1,2,4 ]
 	#case_path[0] = '/a2_'+str(alpha2)
 	#if not os.path.exists( data_path+case_path[0] ):
 		#os.mkdir( data_path+case_path[0] )
-for case in range(1):
+for case in range(2):
 	case_path[1] = '/case_'+str(case)
 	if not os.path.exists( data_path+case_path[0]+case_path[1] ):
 		os.mkdir( data_path+case_path[0]+case_path[1] )
@@ -56,7 +56,7 @@ for case in range(1):
 				#os.mkdir( data_path+case_path[0]+case_path[1]+case_path[2]+case_path[3] )
 		os.chdir( data_path+case_path[0]+case_path[1]+case_path[2]+case_path[3] )
 		os.system(' rm ./* -r -v  ')
-		case_para = ' --nx='+str(64*n+1)+' --ny='+str(32*n+1)+' --nz='+str(64*n+1)+' --npx='+str(n)+' --npy='+str( max(n/2,1) )+' --npz='+str(n)+' '
+		case_para = ' --nx='+str(32*n+1)+' --ny='+str(16*n+1)+' --nz='+str(32*n+1)+' --npx='+str(n)+' --npy='+str( max(n/2,1) )+' --npz='+str(n)+' --maxGrids='+str(2)+' '
 		#os.system( exe_pre(npx*npy*npt,' -R lustre ')+exe_path+exe+case_para+case_consts )
 		print( exe_pre(n*n*max(n/2,1))+exe_path+exe+case_para+case_consts[case] +' > output ' )
 		os.system( exe_pre(n*n*max(n/2,1))+exe_path+exe+case_para+case_consts[case] +' > output ' )
