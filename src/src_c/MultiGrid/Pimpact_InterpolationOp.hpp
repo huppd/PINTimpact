52ce3c06c234752f082a67b593fe2ed2e6f4da25
Daniel Hupp <huppd@inf.ethz.ch>
2015-07-16 15:43:51 +0000

    fixed dd

--- src/src_c/MultiGrid/Pimpact_InterpolationOp.hpp
+++ src/src_c/MultiGrid/Pimpact_InterpolationOp.hpp
@@ -163,7 +163,7 @@ protected:
 				nGatherTotal *= nGather_[i]; // check
 
 				iimax_[i] = (spaceC_->nLoc(i) - 1)/nGather_[i] + 1; // check
-				dd_[i] = (spaceF_->nLoc(i) - 1)/( iimax_[i] -1 ); // check
+				dd_[i] = std::max( (spaceF_->nLoc(i) - 1)/( iimax_[i] -1 ), (Ordinal)1 ); // check
 
 				iiShift_[i] = ( iimax_[i] - 1 )*( ( spaceF_->procCoordinate()[i] -1 )%nGather_[i] ); // check
 			}
