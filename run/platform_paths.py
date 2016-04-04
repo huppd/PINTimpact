ea797c5d0e208b282e202240e80dc15f3db6b0aa
Daniel Hupp <huppd@inf.ethz.ch>
2016-04-04 09:45:19 +0200

    I don't get it

--- run/platform_paths.py
+++ run/platform_paths.py
@@ -1,3 +1,5 @@
+import os
+
 # exection path
 exe_path = "/cluster/home04/math/huppd/PImpact/release/src_c"
 
@@ -5,5 +7,20 @@ exe_path = "/cluster/home04/math/huppd/PImpact/release/src_c"
 data_path = "/cluster/home04/math/huppd/data"
 
 # exection command
-def exe_pre( np, ops ):
-	return( "bsub -n "+str(np)+" "+ops )
+def exe_pre( np, ops='', run=0 ):
+	# return( "bsub -n "+str(np)+" "+ops+" -o output"+str(run)+" " )
+	return( "bsub -n "+str(np)+ ' '+ops+' '+' -oo output'+str(run)+' ' )
+
+def mkdir( path, npa ):
+	fullPath = data_path
+	for i in range( npa+1 ):
+		fullPath += path[i]
+	if not os.path.exists( fullPath ):
+		os.mkdir( fullPath )
+
+def chdir( path, npa ):
+	fullPath = data_path
+	for i in range( npa+1 ):
+		fullPath += path[i]
+	os.chdir( fullPath )
+	os.system( ' rm ./* -r -v  ' )
