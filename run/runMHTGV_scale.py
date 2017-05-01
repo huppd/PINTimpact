41b6978407fb3822bfc06e2b7adb8fa276c1e77f
Daniel Hupp <huppd@inf.ethz.ch>
2017-05-01 12:03:31 +0200

    pretty up

--- run/runMHTGV_scale.py
+++ run/runMHTGV_scale.py
@@ -11,6 +11,7 @@ tree = ET.parse('../XML/parameterTGV.xml')
 root = tree.getroot()
 
 ma.setParameter( root, 'withoutput', 0 )
+ma.setParameter( root, 'initial guess', 'disturbed' )
 # ma.setParameter( root, 'refinement level', 1 )
 
 # make executable ready
@@ -21,17 +22,17 @@ os.system( 'make '+exe+' -j4' )
 
 case_path = ['','','','','','','','','']
 
-runs = range( 0, 1 )
+runs = range( 0, 5 )
 
 res = [  1,10,100 ]
-a2  = [0.1, 1, 10 ]
+a2  = [0.1, 1., 10. ]
 
 re = 1
-a2 = 1
+a2 = 1.
 
-npx = [1,2,2,4,4,4]
-npy = [1,1,2,4,4,4]
-npf = [1,1,1,1,2,3]
+npx = [1,2,2,4,4,4,4]
+npy = [1,1,2,2,4,4,4]
+npf = [1,1,1,1,1,2,3]
 
 
 case_path[0] = '/MHTGV_re_'+str(re)+'_a2_'+str(a2)
@@ -55,10 +56,12 @@ for i in range(len(npx)):
 	ma.setParameter( root, 'npf', npf[i] )
 	tree.write( 'parameter3D.xml' )
 	nptot = npx[i]*npy[i]*npf[i]
+	mem = int( max( 1024, 16*1024/nptot ) )
 	for run in runs:
 		print()
 		print( case_path )
 		# print(     exe_pre( nptot, ' -N -R "select[model==Opteron6174"] -R "rusage[mem='+str(1024*1)+']" ', run ) + exe_path+'/'+exe  )
 		# os.system( exe_pre( nptot, ' -N -R "select[model==Opteron6174"] -R "rusage[mem='+str(1024*1)+']" ', run ) + exe_path+'/'+exe  )
-		print(     exe_pre( nptot, ' -N -R beta -R "span[ptile=4]"  ', run ) + exe_path+'/'+exe  )
-		os.system( exe_pre( nptot, ' -N -R beta -R "span[ptile=4]"  ', run ) + exe_path+'/'+exe  )
+		exeString = exe_pre( nptot, ' -N -R beta -R "span[ptile=4]" -R "rusage[mem=' +str(mem) + ']" ', run ) + exe_path+'/'+exe
+		print( exeString  )
+		os.system( exeString )
