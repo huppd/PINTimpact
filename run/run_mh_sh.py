1a7c3d2a1ac0698081605fb5d97c912d944b198b
Daniel Hupp <huppd@inf.ethz.ch>
2017-09-20 09:47:17 +0200

    bla

--- run/run_mh_sh.py
+++ run/run_mh_sh.py
@@ -26,7 +26,7 @@ STS = [1./60., 1./30., 1./10.]
 
 st = 1./30.
 
-NF = 2
+NF = 1
 
 
 CASE_PATH = ['']*3
@@ -40,7 +40,7 @@ pp.chdir(CASE_PATH, 0)
 NPX = 1
 NPY = 8
 NPZ = 8
-NPF = 2
+NPF = 1
 #
 LXO = 22.5
 LYO = 600.
@@ -97,7 +97,7 @@ nptot = NPX*NPY*NPZ*NPF
 memtot = int(1024.*max(30/nptot, 2))
 print()
 print(CASE_PATH)
-EXE_STRING = pp.exe_pre(nptot, ' -N -W 24:00 ' +
+EXE_STRING = pp.exe_pre(nptot, ' -N -W 12:00 ' +
                         '-R "rusage[mem=' + str(memtot) +
                         ']" ') + pp.EXE_PATH + '/'+EXE
 print(EXE_STRING)
